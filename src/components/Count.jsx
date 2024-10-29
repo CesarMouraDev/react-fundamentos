ccb4ff362d4c191d6eb378ee29b87856e0546057
cesarmouradev <cesarmouradeveloper@gmail.com>
2024-10-29 15:12:54 -0300

    Feat: Count.jsx
    Jamile você é genial!

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const handleIncrement = () => {
-        const newCount = count + Math.PI;        
-        setCount(parseFloat(newCount.toFixed(2)))
-        };
+  const handleIncrement = () => {
+    setCount((prevCount) => parseFloat((prevCount + Math.PI).toFixed(2)));
+  };
 
-    const handleDecrement = () => {
-      const newCount = count - Math.PI;        
-       setCount(parseFloat(newCount.toFixed(2)))
-       };
+  const handleDecrement = () => {
+    setCount((prevCount) => parseFloat((prevCount - Math.PI).toFixed(2)));
+  };
 
-    
-
-    return (
-        <div>
-            <p>voce clicou {count.toFixed(2)} vezes</p>
-            <button onClick={handleIncrement}>Aumentar</button>
-           <button onClick={handleDecrement}>Diminuir</button>
-        </div>
-    );
+  return (
+    <div>
+      <p>Você clicou {count} vezes</p>
+      <button onClick={handleIncrement}>Aumentar</button>
+      <button onClick={handleDecrement}>Diminuir</button>
+    </div>
+  );
 };
 
 export default Counter;
